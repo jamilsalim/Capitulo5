be211f88b93947f44e5802755b73cab86bd6fe00
jdaruix <jamil_salim_daruix@hotmail.com>
2016-03-28 19:23:50 -0300

    .

--- app/src/main/java/com/example/jdaruix/capitulo5/MainActivity.java
+++ app/src/main/java/com/example/jdaruix/capitulo5/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.jdaruix.capitulo5;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //return super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        //MenuItem menuItem = menu.findItem(R.id.action_search);
+        //SearchView searchView = (SearchView) menuItem.getActionView();
+        //searchView.setOnQueryTextListener(onSearch());
+        //return super.onCreateOptionsMenu(menu);
         return  true;
+    }
+
+    private SearchView.OnQueryTextListener onSearch() {
+        return new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                alerta("Buscar o texto: " + query);
+                return  false;
+
+            }
+            @Override
+            public boolean onQueryTextChange(String newText){
+                alerta("Buscar o texto: ");
+                return false;
+            }
+        };
     }
 
     @Override
